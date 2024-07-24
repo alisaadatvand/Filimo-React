cafab2ec5d79ac0a26e7ff9bfcfbaf12e146101e
Ali Saadatvand <ali.saadatvand@gmail.com>
2024-07-24 08:45:54 +0330

    change login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,33 +1,40 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
+  let navigate = useNavigate();
+  let [user, setUser] = useState(null);
 
-    const auth = ()=>{
-        let username = document.querySelector("input[type=text]")
-        let password = document.querySelector("input[type=password]")
-        console.log(username.value,password.value);
+  const auth = () => {
+    let username = document.querySelector("input[type=text]");
+    let password = document.querySelector("input[type=password]");
+    console.log(username.value, password.value);
 
-        if(username==="admin" && password==="admin"){
-            Navigate("/")
-        }
+    if (username.value === "admin" && password.value === "admin") {
+      console.log("hi");
+      setUser("admin");
+      navigate("/");
+      console.log(user);
+    } else {
+      console.log("bye");
     }
+  };
 
-    return ( 
+  return (
+    <>
+      <div className="login">
+        <h2>ورود</h2>
+        <form action="#" method="">
+          <label htmlFor="userName">نام کاربری</label>
+          <input type="text" name="userName" id="userName" />
+          <label htmlFor="password">رمز عبور</label>
+          <input type="password" name="password" id="password" />
+        </form>
+        <button className=" btn-login" onClick={auth}>ورود</button>
+      </div>
+    </>
+  );
+};
 
-        <>
-            <div className="login">
-                <h2>ورود</h2>
-                <form action="#" method="">
-                    <label htmlFor="userName">نام کاربری</label>
-                    <input type="text" name="userName" id="userName" />
-                    <label htmlFor="password">رمز عبور</label>
-                    <input type="password" name="password" id="password" />
-                    <input type="submit" value="ورود" onClick={auth}/>
-                </form>
-            </div>
-        </>
-     );
-}
- 
 export default Login;
